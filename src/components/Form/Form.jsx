92f6a84b892614c67d29669c23e44bf536e95c2d
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-09-07 16:50:13 +0300

    task 2

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,24 +5,29 @@ export class ContactForm extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
-  handleChange = ({ target: { value } }) => {
+  handleChangeName = ({ target: { value } }) => {
     this.setState({ name: value });
+  };
+  handleChangeNumber = ({ target: { value } }) => {
+    this.setState({ number: value });
   };
 
   handleFormSubmit = event => {
     event.preventDefault();
-    const { name } = this.state;
+    const { name, number } = this.state;
 
     if (name.trim() === '') {
       alert('Please enter a valid name.');
       return;
     }
-    const newContact = { id: nanoid(), name };
+    const newContact = { id: nanoid(), name, number };
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
       name: '',
+      number: '',
     }));
   };
 
@@ -30,23 +35,36 @@ export class ContactForm extends Component {
     return (
       <div>
         <form onSubmit={this.handleFormSubmit}>
-        <label htmlFor="name">Name</label>
+          <label htmlFor="name">Name</label>
           <input
             type="text"
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            onChange={this.handleChange}
+            onChange={this.handleChangeName}
             value={this.state.name}
+          />
+          <label htmlFor="number">Number</label>
+          <input
+            type="tel"
+            name="number"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleChangeNumber}
+            value={this.state.number}
           />
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
         <ul>
-            {this.state.contacts.map(contact => (
-                <li key={contact.id} id={contact.id} name={contact.name}>{contact.name}</li>
-            ))}
+          {this.state.contacts.map(contact => (
+            <li key={contact.id} id={contact.id} name={contact.name}>
+              {contact.name}:
+              {contact.number}
+            </li>
+          ))}
         </ul>
       </div>
     );
