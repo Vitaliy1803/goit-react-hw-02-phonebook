3abd8aae199a83ab43ea302edaf2b7f1e73b6260
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-09-08 11:14:21 +0300

    task 3

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,17 +3,31 @@ import { nanoid } from 'nanoid';
 
 export class ContactForm extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '',
+    filter: '',
   };
 
-  handleChangeName = ({ target: { value } }) => {
-    this.setState({ name: value });
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({ [name]: value });
   };
-  handleChangeNumber = ({ target: { value } }) => {
-    this.setState({ number: value });
+
+  findContact = event => {
+    this.setState({ filter: event.currentTarget.value });
   };
+
+  getFilteredContacts = () => {
+    const lowerCaseFilter = this.state.filter.toLowerCase();
+    return this.state.contacts.filter(contact => 
+        contact.name.toLowerCase().includes(lowerCaseFilter)
+    );
+  }
 
   handleFormSubmit = event => {
     event.preventDefault();
@@ -32,6 +46,7 @@ export class ContactForm extends Component {
   };
 
   render() {
+    const filteredContacts = this.getFilteredContacts();
     return (
       <div>
         <form onSubmit={this.handleFormSubmit}>
@@ -42,7 +57,7 @@ export class ContactForm extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            onChange={this.handleChangeName}
+            onChange={this.handleChange}
             value={this.state.name}
           />
           <label htmlFor="number">Number</label>
@@ -52,17 +67,27 @@ export class ContactForm extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            onChange={this.handleChangeNumber}
+            onChange={this.handleChange}
             value={this.state.number}
           />
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
+        <label htmlFor="аfilter">Find contacts by name</label>
+        <input
+          type="text"
+          name="filter"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={this.findContact}
+          value={this.state.filter}
+          
+        />
         <ul>
-          {this.state.contacts.map(contact => (
+          {filteredContacts.map(contact => (
             <li key={contact.id} id={contact.id} name={contact.name}>
-              {contact.name}:
-              {contact.number}
+              {contact.name}:{contact.number}
             </li>
           ))}
         </ul>
