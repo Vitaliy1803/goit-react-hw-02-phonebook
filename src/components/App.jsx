e90d5912aa8d7e6c023950741400b0efd8d2dd67
Vitaliy1803 <35754722+Vitaliy1803@users.noreply.github.com>
2023-09-11 13:29:43 +0300

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -52,6 +52,20 @@ export class App extends Component {
     this.setState({ filter: event.currentTarget.value });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if(parsedContacts) {
+      this.setState({contacts: parsedContacts});
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const filteredContacts = this.getFilteredContacts();
     return (
